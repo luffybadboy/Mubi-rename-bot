a9067a241b226d464a902bfab61949295256dc4b
luffybadboy <luffybadboy17@gmail.com>
2026-05-09 14:25:23 +0530

    Update start_&_cb.py

--- plugins/start_&_cb.py
+++ plugins/start_&_cb.py
@@ -44,19 +44,17 @@ async def start(client, message: Message):
 
     # Send start message with or without picture
     if Config.START_PIC:
-    await message.reply_photo(
-        Config.START_PIC,
-        caption=Txt.START_TXT.format(user.mention),
-        reply_markup=buttons,
-        parse_mode="html"   # 👈 added
-    )
-else:
-    await message.reply_text(
-        text=Txt.START_TXT.format(user.mention),
-        reply_markup=buttons,
-        disable_web_page_preview=True,
-        parse_mode="html"   # 👈 added
-    )
+        await message.reply_photo(
+            Config.START_PIC,
+            caption=Txt.START_TXT.format(user.mention),
+            reply_markup=buttons
+        )
+    else:
+        await message.reply_text(
+            text=Txt.START_TXT.format(user.mention),
+            reply_markup=buttons,
+            disable_web_page_preview=True
+        )
 
 
 # Callback Query Handler
